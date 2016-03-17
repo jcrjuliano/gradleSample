a1a6618d9fd213c7b93b783d83c82a82a5189215
jcrjuliano <jcrjuliano@gmail.com>
2016-03-16 21:22:56 -0300

    Added files via upload

---
+++ gradleSample/src/main/java/gradleSample/Main.java
@@ -0,0 +1,10 @@
+package gradleSample;
+
+import static spark.Spark.*;
+
+public class Main {
+	 public static void main(String[] args) {
+	        get("/hello", (req, res) -> "Hello World");
+	    }
+
+}

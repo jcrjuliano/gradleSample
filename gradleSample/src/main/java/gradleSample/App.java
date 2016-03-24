046e596269ad344bfe60ebb8b154679150ccc5f1
jcrjuliano <jcrjuliano@gmail.com>
2016-03-23 21:12:08 -0300

    Added files via upload

---
+++ gradleSample/src/main/java/gradleSample/App.java
@@ -0,0 +1,18 @@
+package gradleSample;
+
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+
+public class App {
+	 public static void main(String[] args) {
+		  port(getHerokuAssignedPort());
+	      get("/hello", (req, res) -> "Hello World");
+	    }
+	 static int getHerokuAssignedPort() {
+	        ProcessBuilder processBuilder = new ProcessBuilder();
+	        if (processBuilder.environment().get("PORT") != null) {
+	            return Integer.parseInt(processBuilder.environment().get("PORT"));
+	        }
+	        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	    }
+}
